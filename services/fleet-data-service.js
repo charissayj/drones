661147bcdfdd243e2bcd2c23535ed6c7a938b87b
Charissa Johnson <charissa.y.johnson@gmail.com>
2019-08-25 16:58:46 -0500

    add a fleet-data-service class

---
+++ services/fleet-data-service.js
@@ -0,0 +1,9 @@
+import { Car } from "../classes/car.js"
+import { Drone } from "../classes/drone.js"
+
+export class FleetDataService {
+  constructor(){
+    this.cars = [];
+    this.drones = [];
+  }
+}
