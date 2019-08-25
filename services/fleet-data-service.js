70b19a4be6d8331ba6827cdeb0feae1317800fc7
Charissa Johnson <charissa.y.johnson@gmail.com>
2019-08-25 17:37:49 -0500

    add error handler class

--- services/fleet-data-service.js
+++ services/fleet-data-service.js
@@ -1,14 +1,16 @@
-import { Car } from "../classes/car.js"
-import { Drone } from "../classes/drone.js"
+import { Car } from "../classes/car.js";
+import { Drone } from "../classes/drone.js";
+import { DataError } from "../services/data-error.js";
 
 export class FleetDataService {
-  constructor(){
+  constructor() {
     this.cars = [];
     this.drones = [];
+    this.errors = [];
   }
 
   loadData(fleet) {
-    for (let data of fleet){
+    for (let data of fleet) {
       switch (data.type) {
         case "car":
           let car = this.loadCar(data);
@@ -18,21 +20,35 @@ export class FleetDataService {
           let drone = this.loadDrone(data);
           this.drones.push(drone);
           break;
+        default:
+          let error = new DataError("Invalid vehicle type:", data);
+          this.errors.push(error);
+          break;
       }
     }
   }
 
-  loadCar(car){
-    let newCar = new Car(car.license, car.model, car.latlong);
-    newCar.miles = car.miles;
-    newCar.make = car.make
-    return newCar;
+  loadCar(car) {
+    try {
+      let newCar = new Car(car.license, car.model, car.latlong);
+      newCar.miles = car.miles;
+      newCar.make = car.make;
+      return newCar;
+    } catch (error) {
+      this.errors.push(new DataError("error loading car", car));
+    }
+    return null;
   }
 
-  loadDrone(drone){
-    let newDrone = new Drone(drone.license, drone.model, drone.latlong);
-    newDrone.airTimeHours = drone.airTimeHours;
-    newDrone.base = drone.base;
-    return newDrone;
+  loadDrone(drone) {
+    try {
+      let newDrone = new Drone(drone.license, drone.model, drone.latlong);
+      newDrone.airTimeHours = drone.airTimeHours;
+      newDrone.base = drone.base;
+      return newDrone;
+    } catch (error) {
+      this.errors.push(new DataError("error loading drone", drone));
+    }
+    return null;
   }
 }
