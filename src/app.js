776e945984ff98b26fa3ca443da602e69766b4c7
Charissa Johnson <charissa.y.johnson@gmail.com>
2019-08-25 15:05:15 -0500

    create class Drone

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("in app.js");
+class Drone {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+
+let drone = new Drone("A123", "Flyer");
+console.log(drone instanceof Drone);
+console.log(drone.id);
+console.log(drone.name);
