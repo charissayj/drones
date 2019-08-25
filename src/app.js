bd04a65844112bb0f6b05bfa5a6442e14922dfca
Charissa Johnson <charissa.y.johnson@gmail.com>
2019-08-25 15:13:27 -0500

    Basic class principles

--- src/app.js
+++ src/app.js
@@ -3,10 +3,30 @@ class Drone {
     this.id = id;
     this.name = name;
   }
+
+  static getCompany() {
+    console.log("in getCompany");
+  }
+
+  fly() {
+    console.log(`Drone ${this.id} is flying`);
+  }
 }
+
+// Static properties
+Drone.maxHeight = 2000;
 
 
 let drone = new Drone("A123", "Flyer");
+let drone2 = new Drone("B123", "DroneB");
 console.log(drone instanceof Drone);
 console.log(drone.id);
 console.log(drone.name);
+console.log(drone2.id);
+console.log(drone2.name);
+console.log(Drone.maxHeight);
+
+drone.fly()
+drone2.fly();
+
+Drone.getCompany();
